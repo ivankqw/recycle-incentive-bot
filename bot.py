7ac35d199499e071ea2d7653116b769d694a08cb
ivan <ivankohquanwei@gmail.com>
2022-07-10 05:11:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,11 +33,13 @@ def help(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(s1 + s2 + s3 + s4,
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
+
 def start(update: Update, context: CallbackContext) -> None:
     s = "Welcome to the Recycle Incentive Bot! We are glad to have you here today 🌻🌼🌷🌸💮🌹🥀\n\n Click on /help to find out more about this bot or get started by clicking on the following buttons!"
     reply_keyboard = [["/cashfortrash", "/ewaste"]]
     update.message.reply_text(s,
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True), parse_mode='HTML')
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+                              parse_mode='HTML')
 
 
 # entry points
@@ -75,28 +77,34 @@ def location(update: Update, context: CallbackContext) -> None:
     message = update.message
     curr_longitude = message.location['longitude']
     curr_latitude = message.location['latitude']
-    # message.reply_text(f'so you are at {curr_longitude}, {curr_latitude}, yes?')
+    directions_base_url = 'https://www.google.com/maps/dir/?api=1&destination='
 
-    df = pd.read_excel(data_path, sheet_name="cash_for_trash", converters={'updated_time_start': str, 'updated_time_end': str})
+    df = pd.read_excel(data_path, sheet_name="cash_for_trash",
+                       converters={'updated_time_start': str, 'updated_time_end': str})
     df['updated_time_start'] = df['updated_time_start'].apply(lambda x: "0" + str(x) if len(str(x)) == 3 else str(x))
     df['updated_time_end'] = df['updated_time_end'].apply(lambda x: "0" + str(x) if len(str(x)) == 3 else str(x))
     df['distances'] = df.apply(lambda x: distance(x['Longitude'], x['Latitude'], curr_longitude, curr_latitude), axis=1)
     result_df = df.sort_values('distances').iloc[0:5]
+
+    long_list = result_df['Longitude'].tolist()
+    lat_list = result_df['Latitude'].tolist()
+    directions_list = [directions_base_url + str(x) + "," + str(y) for x, y in zip(lat_list, long_list)]
     address_list = result_df['Address'].tolist()
     day_list = result_df['Day'].tolist()
     time_start_list = result_df['updated_time_start'].tolist()
     time_end_list = result_df['updated_time_end'].tolist()
-    s = ''
+    s = 'Here are your current top 5 nearest Cash For Trash locations! 🚮😸'
     for i in range(5):
-        s += f'\n\n {i+1}. \n address: {address_list[i]} \n day: {day_list[i]} \n start: {time_start_list[i]} \n end: {time_end_list[i]}'
+        s += f'\n\n {i + 1}. \n Address: {address_list[i]} \n Collection day(s): {day_list[i]} \n Start Time: {time_start_list[i]} \n End Time: {time_end_list[i]} \n\n Get Directions: {directions_list[i]}'
     message.reply_text(s)
 
 
 def distance(lon1, lat1, lon2, lat2):
     R = 6371000  # radius of the Earth in m
-    x = (lon2 - lon1) * cos(0.5*(lat2+lat1))
+    x = (lon2 - lon1) * cos(0.5 * (lat2 + lat1))
     y = (lat2 - lat1)
-    return (2*pi*R/360) * sqrt( x*x + y*y )
+    return (2 * pi * R / 360) * sqrt(x * x + y * y)
+
 
 def main() -> None:
     token = os.getenv("TOKEN")
