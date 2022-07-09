3bfab5db06d607658a0ed5edfe3e3879643b41a8
valentinkhoo <valentin_khoo@outlook.com>
2022-07-10 07:30:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -110,7 +110,8 @@ def location(update: Update, context: CallbackContext) -> None:
     for i in range(5):
         s += f'\n\n {i + 1}. \n Address: {address_list[i]} \n Collection day(s): {day_list[i]} \n Start Time: {time_start_list[i]} \n End Time: {time_end_list[i]} \n\n Get Directions: {directions_list[i]}'
     message.reply_text(s)
-    update.message.edit_message_reply_markup(None)
+
+    
     return ConversationHandler.END
 
 def location_ewaste(update: Update, context: CallbackContext) -> None:
@@ -139,7 +140,7 @@ def location_ewaste(update: Update, context: CallbackContext) -> None:
         for i in range(5):
             s += f'\n\n {i + 1}. \n Address: {address_list[i]} \n \n\n Get Directions: {directions_list[i]}'
     message.reply_text(s)
-    update.message.edit_message_reply_markup(None)
+    
     return ConversationHandler.END
 
 
